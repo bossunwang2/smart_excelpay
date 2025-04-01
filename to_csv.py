8315c22d7a01ab7e17a32bdcc53f15c5a7b778e0
bossunwang2 <bossun.wang@xrspace.io>
2025-04-01 16:21:34 +0800

    feat: update file name and file

--- to_csv.py
+++ to_csv.py
@@ -8,6 +8,6 @@ def process(file_path, output_csv):
 
 
 if __name__ == '__main__':
-    file_path = "cycling_data_round1.txt"
-    output_csv = "cycling_data_round1.csv"
+    file_path = "cycling_data_round2.txt"
+    output_csv = "cycling_data_round2.csv"
     process(file_path, output_csv)
