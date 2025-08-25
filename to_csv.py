8e82c2621e5a3a7d64c1209e73ec333d392ccd10
bossunwang2 <bossun.wang@xrspace.io>
2025-08-25 11:22:53 +0800

    feat: update to extract round5 data

--- to_csv.py
+++ to_csv.py
@@ -8,6 +8,6 @@ def process(file_path, output_csv):
 
 
 if __name__ == '__main__':
-    file_path = "cycling_data_round4.txt"
-    output_csv = "cycling_data_round4.csv"
+    file_path = "cycling_data_round5.txt"
+    output_csv = "cycling_data_round5.csv"
     process(file_path, output_csv)
